fd832b48f9f01cbc4c1c0f27d16f2c37032ad21a
Juan Cazala <juan@decentraland.org>
2018-10-29 12:09:58 -0300

    fix: remove story

--- src/components/Segment/Segment.stories.tsx
+++ src/components/Segment/Segment.stories.tsx
@@ -1,25 +1,25 @@
-import * as React from 'react'
-import { storiesOf } from '@storybook/react'
-import { Header, Segment } from '../..'
+// import * as React from 'react'
+// import { storiesOf } from '@storybook/react'
+// import { Header, Segment } from '../..'
 
-const styles = {
-  width: 1024,
-  height: 420
-}
+// const styles = {
+//   width: 1024,
+//   height: 420
+// }
 
-storiesOf('Segment', module).add('Single segment', () => (
-  <div style={styles}>
-    <Segment style={{ width: 400 }}>
-      <Header>Title</Header>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-    </Segment>
-  </div>
-))
+// storiesOf('Segment', module).add('Single segment', () => (
+//   <div style={styles}>
+//     <Segment style={{ width: 400 }}>
+//       <Header>Title</Header>
+//       <p>
+//         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+//         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+//         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+//         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+//         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+//         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+//         mollit anim id est laborum.
+//       </p>
+//     </Segment>
+//   </div>
+// ))
