d94c4a0ff48e6459f45a825ed1dfa1180d314916
Juan Cazala <juan@decentraland.org>
2018-10-29 12:04:41 -0300

    fix: tests

--- src/components/Segment/Segment.stories.tsx
+++ src/components/Segment/Segment.stories.tsx
@@ -2,17 +2,24 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Header, Segment } from '../..'
 
+const styles = {
+  width: 1024,
+  height: 420
+}
+
 storiesOf('Segment', module).add('Single segment', () => (
-  <Segment style={{ width: 400 }}>
-    <Header>Title</Header>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
-  </Segment>
+  <div style={styles}>
+    <Segment style={{ width: 400 }}>
+      <Header>Title</Header>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </Segment>
+  </div>
 ))
