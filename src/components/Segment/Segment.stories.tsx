fe951da18919e640c580f0b0360288005548aa50
Juan Cazala <juan@decentraland.org>
2018-10-29 11:30:53 -0300

    fix(Segment): border

--- src/components/Segment/Segment.stories.tsx
+++ src/components/Segment/Segment.stories.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Header, Segment } from '../..'
 
-storiesOf('Segment', module).add('Just a segment', () => (
+storiesOf('Segment', module).add('Single segment', () => (
   <Segment style={{ width: 400 }}>
     <Header>Title</Header>
     <p>
