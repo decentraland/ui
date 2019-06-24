a1065e4161d6e333cd41372713499c11ed38234b
Juan Cazala <juan@decentraland.org>
2019-06-24 10:24:25 -0300

    chore: added old logo url

--- postbuild.js
+++ postbuild.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
 
-const path = './storybook-static/index.html'
+const path = require('path').resolve(__dirname, './storybook-static/index.html')
 
 let html = fs.readFileSync(path, 'utf-8')
 
 html = html.replace('Storybook', 'UI | Decentraland')
 
 fs.writeFileSync(path, html)
+
+console.log(`Replaced "Storybook" with "UI | Decentraland" in ${path}`)
