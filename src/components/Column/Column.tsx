d70f8d6234794fed59121ef4697d10f8021c5503
Juan Cazala <juan@decentraland.org>
2020-05-06 21:06:28 -0300

    feat: improved Row and Column

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -2,24 +2,54 @@ import * as React from 'react'
 import './Column.css'
 
 export type ColumnProps = {
+  align: 'left' | 'center' | 'right'
   center?: boolean
+  width?: number
+  height?: number
   className?: string
   onClick?: (event: React.MouseEvent<HTMLDivElement>) => void
 }
 
 export class Column extends React.PureComponent<ColumnProps> {
   static defaultProps: Partial<ColumnProps> = {
+    align: 'left',
     center: false,
     className: ''
   }
 
   render() {
-    const { children, className, center, onClick } = this.props
+    const {
+      children,
+      className,
+      center,
+      align,
+      width,
+      height,
+      onClick
+    } = this.props
+    const classes = ['dcl', 'row']
+    if (center) {
+      console.warn(
+        `The prop 'center' has been deprecated from <Row> component, use aligh="center" instead.`
+      )
+      classes.push('center')
+    } else {
+      classes.push(align)
+    }
+    if (className) {
+      classes.push(className)
+    }
+
+    let style: React.CSSProperties = {}
+    if (width) {
+      style.width = width
+    }
+    if (height) {
+      style.height = height
+    }
+
     return (
-      <div
-        className={`dcl column ${center ? 'center' : ''} ${className}`}
-        onClick={onClick}
-      >
+      <div className={classes.join(' ')} onClick={onClick} style={style}>
         {children}
       </div>
     )
--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -3,23 +3,54 @@ import './Row.css'
 
 export type RowProps = {
   center?: boolean
+  align?: 'left' | 'center' | 'right'
+  width?: number
+  height?: number
   className?: string
   onClick?: (event: React.MouseEvent<HTMLDivElement>) => void
 }
 
 export class Row extends React.PureComponent<RowProps> {
   static defaultProps: Partial<RowProps> = {
+    align: 'left',
     center: false,
     className: ''
   }
 
   render() {
-    const { children, className, onClick, center } = this.props
+    const {
+      children,
+      className,
+      onClick,
+      align,
+      center,
+      width,
+      height
+    } = this.props
+
+    const classes = ['dcl', 'row']
+    if (center) {
+      console.warn(
+        `The prop 'center' has been deprecated from <Row> component, use aligh="center" instead.`
+      )
+      classes.push('center')
+    } else {
+      classes.push(align)
+    }
+    if (className) {
+      classes.push(className)
+    }
+
+    let style: React.CSSProperties = {}
+    if (width) {
+      style.width = width
+    }
+    if (height) {
+      style.height = height
+    }
+
     return (
-      <div
-        className={`dcl row ${center ? 'center' : ''} ${className}`}
-        onClick={onClick}
-      >
+      <div className={classes.join(' ')} onClick={onClick} style={style}>
         {children}
       </div>
     )
