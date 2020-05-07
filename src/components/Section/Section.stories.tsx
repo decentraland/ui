15e3865987438cf055726d80c8b6ee7f3593fc76
Juan Cazala <juan@decentraland.org>
2020-05-06 21:07:18 -0300

    feat: added Section

---
+++ src/components/Section/Section.stories.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import centered from '@storybook/addon-centered/react'
+import { Section } from './Section'
+import { Header } from '../../index'
+
+storiesOf('Section', module)
+  .addDecorator(centered)
+  .add('Example', () => (
+    <>
+      <Section>
+        <Header sub>Section 1</Header>
+        This is a section
+      </Section>
+      <Section>
+        <Header sub>Section 2</Header>
+        This is another section
+      </Section>
+    </>
+  ))
---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import './Section.css'
+
+export type SectionProps = {
+  className?: string
+  size?: 'small' | 'medium' | 'large'
+}
+
+export class Section extends React.PureComponent<SectionProps> {
+  static defaultProps = {
+    size: 'medium'
+  }
+
+  render() {
+    const { size, className, children } = this.props
+    const classes = ['dcl', 'section', size]
+    if (className) {
+      classes.push(className)
+    }
+    return (
+      <div className={classes.join(' ')}>
+        {children} {size}
+      </div>
+    )
+  }
+}
