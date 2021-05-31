8d425b9e2c9d8f3f14a983022ed333b13f2890c8
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-05-31 12:45:57 -0300

    feat: toast position type

--- src/components/Toasts/Toasts.types.ts
+++ src/components/Toasts/Toasts.types.ts
@@ -1,6 +1,12 @@
 import * as React from 'react'
 
+export type ToastPosition =
+  | 'bottom left'
+  | 'bottom right'
+  | 'top left'
+  | 'top right'
+
 export type Props = {
-  position?: 'bottom left' | 'bottom right' | 'top left' | 'top right'
+  position?: ToastPosition
   children: React.ReactNode
 }
