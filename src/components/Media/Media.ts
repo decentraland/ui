28d1740eae61ac1479d94bfe837b42a09bd08bff
Lautaro Petaccio <lausuper@gmail.com>
2021-08-12 18:09:28 -0300

    feat: Add more JSDocs

--- src/components/Media/Media.ts
+++ src/components/Media/Media.ts
@@ -1,15 +1,30 @@
 import { useMediaQuery } from 'react-responsive'
 
+/**
+ * Media hook to determine if we're going to be rendering in a desktop environment.
+ */
 const useDesktopMediaQuery = (): boolean => useMediaQuery({ minWidth: 992 })
 
+/**
+ * Media hook to determine if we're going to be rendering in a tablet environment.
+ */
 const useTabletMediaQuery = (): boolean =>
   useMediaQuery({ minWidth: 768, maxWidth: 991 })
 
+/**
+ * Media hook to determine if we're going to be rendering in a tablet or below environment.
+ */
 const useTabletAndBelowMediaQuery = (): boolean =>
   useMediaQuery({ maxWidth: 991 })
 
+/**
+ * Media hook to determine if we're going to be rendering in a mobile environment.
+ */
 const useMobileMediaQuery = (): boolean => useMediaQuery({ maxWidth: 767 })
 
+/**
+ * Media hook to determine if we're going to be rendering in an environment that's not mobile.
+ */
 const useNotMobileMediaQuery = (): boolean => useMediaQuery({ minWidth: 768 })
 
 /**
