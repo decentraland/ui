90a67ee0e033f3c06a2fc6452950bc5889457108
Juan Cazala <juancazala@gmail.com>
2021-11-22 19:43:44 -0300

    feat: wearable preview

---
+++ src/components/WearablePreview/WearablePreview.stories.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { WearablePreview } from './WearablePreview'
+import './WearablePreview.stories.css'
+import { Container, Footer, Header, Hero, Navbar, Page } from '../..'
+
+storiesOf('WearablePreview', module)
+  .add('Preview an item', () => (
+    <div className="WearablePreview-story-container">
+      <WearablePreview
+        contractAddress="0xee8ae4c668edd43b34b98934d6d2ff82e41e6488"
+        itemId="5"
+      />
+    </div>
+  ))
+  .add('Preview an token', () => (
+    <div className="WearablePreview-story-container">
+      <WearablePreview
+        contractAddress="0xee8ae4c668edd43b34b98934d6d2ff82e41e6488"
+        tokenId="1"
+      />
+    </div>
+  ))
+  .add('Preview an texture wearable', () => (
+    <div className="WearablePreview-story-container">
+      <WearablePreview
+        contractAddress="0xfeb52cbf71b9adac957c6f948a6cf9980ac8c907"
+        tokenId="3073"
+      />
+    </div>
+  ))
+  .add('Use as Hero', () => (
+    <div className="WearablePreview-story-container">
+      <Navbar isFullscreen activePage="marketplace" />
+      <Page isFullscreen>
+        <Hero height={420}>
+          <WearablePreview
+            contractAddress="0xee8ae4c668edd43b34b98934d6d2ff82e41e6488"
+            itemId="5"
+          />
+        </Hero>
+        <Container>
+          <Header>Hello Wolrd</Header>
+          <p>This page has a hero</p>
+        </Container>
+      </Page>
+      <Footer />
+    </div>
+  ))
---
+++ src/components/WearablePreview/WearablePreview.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import './WearablePreview.css'
+
+export type WearablePreviewProps = {
+  contractAddress: string
+  tokenId?: string
+  itemId?: string
+}
+
+export class WearablePreview extends React.PureComponent<WearablePreviewProps> {
+  static baseUrl = 'http://localhost:3000'
+  render() {
+    const { contractAddress, tokenId, itemId } = this.props
+    const contract = `?contract=${contractAddress}`
+    const token = tokenId ? `&token=${tokenId}` : ''
+    const item = itemId ? `&item=${itemId}` : ''
+    const url = WearablePreview.baseUrl + contract + token + item
+    return (
+      <iframe
+        className="WearablePreview"
+        src={url}
+        width="100%"
+        height="100%"
+        frameBorder="0"
+      ></iframe>
+    )
+  }
+}
