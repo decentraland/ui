d17643539bc4cc6d3931ccd517f989fde2d620f3
Juan Cazala <juancazala@gmail.com>
2021-11-24 14:40:00 -0300

    feat: added onLoad callback

--- src/components/WearablePreview/WearablePreview.stories.tsx
+++ src/components/WearablePreview/WearablePreview.stories.tsx
@@ -22,11 +22,29 @@ storiesOf('WearablePreview', module)
       />
     </div>
   ))
+  .add('Preview a token from ropsten', () => (
+    <div className="WearablePreview-story-container">
+      <WearablePreview
+        contractAddress="0x07e2a8fce1eb1ddf8e7d8de3f86654abe32eb97f"
+        tokenId="1"
+        dev={true}
+      />
+    </div>
+  ))
   .add('Preview a texture wearable', () => (
     <div className="WearablePreview-story-container">
       <WearablePreview
         contractAddress="0xfeb52cbf71b9adac957c6f948a6cf9980ac8c907"
         tokenId="3073"
+      />
+    </div>
+  ))
+  .add('Using onLoad callback', () => (
+    <div className="WearablePreview-story-container">
+      <WearablePreview
+        contractAddress="0xee8ae4c668edd43b34b98934d6d2ff82e41e6488"
+        itemId="5"
+        onLoad={() => console.log('loaded!')}
       />
     </div>
   ))
--- src/components/WearablePreview/WearablePreview.tsx
+++ src/components/WearablePreview/WearablePreview.tsx
@@ -5,16 +5,32 @@ export type WearablePreviewProps = {
   contractAddress: string
   tokenId?: string
   itemId?: string
+  dev?: boolean
+  onLoad?: () => void
 }
 
 export class WearablePreview extends React.PureComponent<WearablePreviewProps> {
   static baseUrl = 'https://wearable-preview.decentraland.org'
+  handleMessage = (event: MessageEvent<string>) => {
+    const { onLoad } = this.props
+    const { origin } = event
+    if (origin === WearablePreview.baseUrl && onLoad) {
+      onLoad()
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('message', this.handleMessage, false)
+  }
+  componentWillUnmount() {
+    window.removeEventListener('message', this.handleMessage, false)
+  }
   render() {
-    const { contractAddress, tokenId, itemId } = this.props
+    const { contractAddress, tokenId, itemId, dev } = this.props
     const contract = `?contract=${contractAddress}`
     const token = tokenId ? `&token=${tokenId}` : ''
     const item = itemId ? `&item=${itemId}` : ''
-    const url = WearablePreview.baseUrl + contract + token + item
+    const env = dev ? `&env=dev` : ''
+    const url = WearablePreview.baseUrl + contract + token + item + env
     return (
       <iframe
         className="WearablePreview"
