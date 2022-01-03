edf0e9816fffca5e7ba7f4453cade59b1363aeae
frami <i@2fd.me>
2022-01-03 18:03:42 -0300

    feat: add provider prop to Loader

--- src/components/Loader/Loader.stories.tsx
+++ src/components/Loader/Loader.stories.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react'
+import { ProviderType } from '@dcl/schemas/dist/dapps/provider-type'
 import { storiesOf } from '@storybook/react'
 import centered from '@storybook/addon-centered/react'
 import { Loader } from './Loader'
+import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid'
 
 storiesOf('Loader', module)
   .addDecorator(centered)
   .add('Main Loader', () => <Loader active size="massive" />)
   .add('Tiny Loader', () => <Loader active size="mini" />)
+  .add('With Provider', () => <Grid stackable container centered>
+    <Grid.Row>
+      <Grid.Column tablet={4}>
+        <Loader active size="massive" provider={ProviderType.INJECTED} />
+      </Grid.Column>
+      <Grid.Column tablet={4}>
+        <Loader active size="massive" provider={ProviderType.WALLET_CONNECT} />
+      </Grid.Column>
+      <Grid.Column tablet={4}>
+        <Loader active size="massive" provider={ProviderType.FORTMATIC} />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>)
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,24 @@
-export {
-  default as Loader,
-  LoaderProps
-} from 'semantic-ui-react/dist/commonjs/elements/Loader/Loader'
+import * as React from 'react'
+import { ProviderType } from '@dcl/schemas/dist/dapps/provider-type'
+import BaseLoader, { LoaderProps as BaseLoaderProps } from 'semantic-ui-react/dist/commonjs/elements/Loader/Loader'
 import './Loader.css'
+
+export interface LoaderProps extends BaseLoaderProps {
+  provider?: ProviderType
+}
+
+export class Loader extends React.PureComponent<LoaderProps> {
+
+  getClassName() {
+    return [
+      this.props.className || '',
+      this.props.provider || '',
+    ]
+      .join(' ')
+      .trim()
+  }
+
+  render(): JSX.Element {
+    return <BaseLoader {...this.props} className={this.getClassName()} />
+  }
+}
