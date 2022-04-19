14acf1ab8a84bd568881abc7a8bcd896a4904e64
Braian Mellor <braianj@gmail.com>
2022-04-19 17:27:35 -0300

    feat: remove the usage of classname function in component Box and ToggleBox (#153)

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import classNames from 'classnames'
 import './Box.css'
 
 export type BoxProps = {
@@ -12,8 +11,13 @@ export class Box extends React.PureComponent<BoxProps> {
   render(): JSX.Element {
     const { className, header, children } = this.props
 
+    const classes = ['dcl', 'box']
+    if (className) {
+      classes.push(className)
+    }
+
     return (
-      <div className={classNames('dcl box', className)}>
+      <div className={classes.join(' ')}>
         {header && <div className={'dcl box-header'}>{header}</div>}
         <div className={'dcl box-children'}>{children}</div>
       </div>
--- src/components/ToggleBox/ToggleBox.tsx
+++ src/components/ToggleBox/ToggleBox.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import classNames from 'classnames'
 import { Box } from '../Box/Box'
 import './ToggleBox.css'
 
@@ -24,23 +23,34 @@ export class ToggleBox extends React.PureComponent<ToggleBoxProps> {
   render(): JSX.Element {
     const { header, className, items, onClick } = this.props
 
+    const classes = ['dcl', 'togglebox-item']
+    if (className) {
+      classes.push(className)
+    }
+
     return (
       <Box header={header} className={className}>
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className={classNames('dcl togglebox-item', {
-              ['active']: !!item.active,
-              ['disabled']: !!item.disabled
-            })}
-            onClick={(event) => !item.disabled && onClick(event, item)}
-          >
-            <div className={'dcl togglebox-item-title'}>{item.title}</div>
-            <div className={'dcl togglebox-item-description'}>
-              {item.description}
+        {items.map((item, index) => {
+          const classesItem = [...classes]
+          if (item.active) {
+            classesItem.push('active')
+          }
+          if (item.disabled) {
+            classesItem.push('disabled')
+          }
+          return (
+            <div
+              key={index}
+              className={classesItem.join(' ')}
+              onClick={(event) => !item.disabled && onClick(event, item)}
+            >
+              <div className={'dcl togglebox-item-title'}>{item.title}</div>
+              <div className={'dcl togglebox-item-description'}>
+                {item.description}
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </Box>
     )
   }
