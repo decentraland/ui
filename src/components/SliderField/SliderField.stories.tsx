f6c5ef0c9474c0705641e861c2f596d8bf4bb4ab
Braian Mellor <braianj@gmail.com>
2022-05-12 16:09:16 -0300

    feat: SliderField and RangeField components were merged

--- src/components/SliderField/SliderField.stories.tsx
+++ src/components/SliderField/SliderField.stories.tsx
@@ -5,7 +5,10 @@ import { SliderField } from './SliderField'
 
 storiesOf('SliderField', module)
   .addDecorator(centered)
-  .add('Simple', () => <SliderField header="SliderField Simple" />)
+  .add('Slider Simple', () => <SliderField header="SliderField Simple" />)
+  .add('Range Simple', () => (
+    <SliderField header="SliderField Simple Range" range={true} />
+  ))
   .add('With Label', () => (
     <SliderField
       header="SliderField Label"
@@ -17,12 +20,29 @@ storiesOf('SliderField', module)
   .add('With Max and Min set', () => (
     <SliderField header="Min set at 200 and Max at 500" min={200} max={500} />
   ))
-  .add('Default Value', () => (
+  .add('Range with Max and Min set', () => (
+    <SliderField
+      header="Min set at 200 and Max at 500"
+      min={200}
+      max={500}
+      range={true}
+    />
+  ))
+  .add('Default value', () => (
     <SliderField
       header="Default value set to 70"
       min={0}
       max={100}
       defaultValue={70}
+    />
+  ))
+  .add('Range default value', () => (
+    <SliderField
+      header="Default value set to 20 - 70"
+      min={0}
+      max={100}
+      defaultValue={[20, 70]}
+      range={true}
     />
   ))
   .add('With onMouseUp', () => (
@@ -33,11 +53,29 @@ storiesOf('SliderField', module)
       onMouseUp={(_, data) => alert(JSON.stringify(data))}
     />
   ))
-  .add('With onChange', () => (
+  .add('Range with onMouseUp', () => (
     <SliderField
       header="SliderField Header"
       min={0}
       max={100}
+      onMouseUp={(_, data) => alert(JSON.stringify(data))}
+      range
+    />
+  ))
+  .add('With onChange', () => (
+    <SliderField
+      header="Shown on console"
+      min={0}
+      max={100}
       onChange={(_, data) => console.log(data)}
+    />
+  ))
+  .add('Range with onChange', () => (
+    <SliderField
+      header="Shown on console"
+      min={0}
+      max={100}
+      onChange={(_, data) => console.log(data)}
+      range
     />
   ))
--- src/components/SliderField/SliderField.tsx
+++ src/components/SliderField/SliderField.tsx
@@ -6,20 +6,34 @@ export type SliderFieldProps = {
   className?: string
   min?: number
   max?: number
-  defaultValue?: number
-  label?: string | React.PureComponent<{ value: number }>
-  onChange?: (ev: React.ChangeEvent<HTMLInputElement>, data: number) => void
-  onMouseUp?: (ev: React.MouseEvent<HTMLInputElement>, data: number) => void
+  defaultValue?: number | readonly [number, number]
+  label?: string | React.PureComponent<{ value: number | [number, number] }>
+  range?: boolean
+  onChange?: (
+    ev: React.ChangeEvent<HTMLInputElement>,
+    data: number | [number, number]
+  ) => void
+  onMouseUp?: (
+    ev: React.MouseEvent<HTMLInputElement>,
+    data: number | [number, number]
+  ) => void
 }
 
+export enum SliderLastInteraction {
+  'from',
+  'to'
+}
 export enum SliderDefault {
   MIN = 0,
   MAX = 100,
-  VALUE = 100
+  FROM = 0,
+  TO = 100
 }
 
 export type SliderFieldState = {
-  value: number
+  from: number
+  to: number
+  lastInteraction: SliderLastInteraction
 }
 
 export class SliderField extends React.PureComponent<
@@ -27,7 +41,9 @@ export class SliderField extends React.PureComponent<
   SliderFieldState
 > {
   state = {
-    value: SliderDefault.VALUE
+    from: SliderDefault.FROM,
+    to: SliderDefault.TO,
+    lastInteraction: SliderLastInteraction.from
   }
 
   componentDidMount(): void {
@@ -35,18 +51,26 @@ export class SliderField extends React.PureComponent<
       this.setState((prevState) => {
         return {
           ...prevState,
-          value: !isNaN(this.props.max) ? this.props.max : prevState.value
+          from: !isNaN(this.props.min) ? this.props.min : prevState.from,
+          to: !isNaN(this.props.max) ? this.props.max : prevState.to
         }
       })
     } else {
-      this.setState({
-        value: this.props.defaultValue
-      })
+      if (Array.isArray(this.props.defaultValue)) {
+        this.setState({
+          from: this.props.defaultValue[0],
+          to: this.props.defaultValue[1]
+        })
+      } else {
+        this.setState({
+          to: Number(this.props.defaultValue)
+        })
+      }
     }
   }
 
   render(): JSX.Element {
-    const { header, className, onChange, onMouseUp } = this.props
+    const { header, className, onChange, onMouseUp, range } = this.props
 
     const min = this.props.min || SliderDefault.MIN
     const max = this.props.max || SliderDefault.MAX
@@ -63,12 +87,35 @@ export class SliderField extends React.PureComponent<
         (prevState) => {
           return {
             ...prevState,
-            value: Number(input.value)
+            from: Math.min(Number(input.value), prevState.to),
+            lastInteraction: SliderLastInteraction.from
           }
         },
         () => {
           if (onChange) {
-            onChange(e, this.state.value)
+            onChange(e, [this.state.from, this.state.to])
+          }
+        }
+      )
+    }
+
+    const handleChangeTo = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const input = e.target
+
+      this.setState(
+        (prevState) => {
+          return {
+            ...prevState,
+            to: Math.max(Number(input.value), prevState.from),
+            lastInteraction: SliderLastInteraction.to
+          }
+        },
+        () => {
+          if (onChange) {
+            onChange(
+              e,
+              range ? [this.state.from, this.state.to] : this.state.to
+            )
           }
         }
       )
@@ -76,38 +123,77 @@ export class SliderField extends React.PureComponent<
 
     const handleMouseUp = (e: React.MouseEvent<HTMLInputElement>) => {
       if (onMouseUp) {
-        onMouseUp(e, this.state.value)
+        onMouseUp(e, range ? [this.state.from, this.state.to] : this.state.to)
       }
     }
 
-    const trackLeft = `${100 - ((this.state.value - min) * 100) / (max - min)}%`
-    const handleValue = `${((this.state.value - min) * 100) / (max - min)}%`
+    const diffMaxMin = max - min
+    const trackLeft = range
+      ? `${((this.state.from - min) * 100) / diffMaxMin}%`
+      : 0
+    const trackRight = range
+      ? `${((max - this.state.to) * 100) / diffMaxMin}%`
+      : `${100 - ((this.state.to - min) * 100) / diffMaxMin}%`
+    const handleFrom = `${((this.state.from - min) * 100) / diffMaxMin}%`
+    const handleTo = `${((this.state.to - min) * 100) / diffMaxMin}%`
 
-    const label = this.props.label || `${this.state.value}`
+    const label =
+      this.props.label ||
+      (range ? `${this.state.from} - ${this.state.to}` : this.state.to)
 
     return (
       <div>
         <div className={classes.join(' ')}>
-          <div className={'dcl slider-header'}>{header}</div>
+          <div className={'dcl sliderfield-header'}>{header}</div>
           <p>{label}</p>
           <div className="dcl sliderfield-wrapper">
             <div className={'dcl sliderfield-rail'}>
               <div
                 className={'dcl sliderfield-track'}
-                style={{ left: 0, right: trackLeft }}
+                style={{ left: trackLeft, right: trackRight }}
               ></div>
+              {range && (
+                <span
+                  className={'dcl sliderfield-handle'}
+                  style={{ left: handleFrom }}
+                ></span>
+              )}
+
               <span
                 className={'dcl sliderfield-handle'}
-                style={{ left: handleValue }}
+                style={{ left: handleTo }}
               ></span>
             </div>
+
+            {range && (
+              <input
+                type="range"
+                value={this.state.from}
+                max={max}
+                min={min}
+                step="1"
+                onChange={handleChangeFrom}
+                style={{
+                  zIndex:
+                    this.state.lastInteraction == SliderLastInteraction.from
+                      ? 4
+                      : 3
+                }}
+                onMouseUp={handleMouseUp}
+              />
+            )}
+
             <input
               type="range"
-              value={this.state.value}
+              value={this.state.to}
               max={max}
               min={min}
               step="1"
-              onChange={handleChangeFrom}
+              onChange={handleChangeTo}
+              style={{
+                zIndex:
+                  this.state.lastInteraction == SliderLastInteraction.to ? 4 : 3
+              }}
               onMouseUp={handleMouseUp}
             />
           </div>
