e543f358db2751f4b8fdcf2d6caec8854140b7d3
Braian Mellor <braianj@gmail.com>
2022-05-13 18:35:14 -0300

    feat: changes in code by PR request

--- src/components/SliderField/SliderField.tsx
+++ src/components/SliderField/SliderField.tsx
@@ -36,6 +36,11 @@ export type SliderFieldState = {
   lastInteraction: SliderLastInteraction
 }
 
+export type SliderFieldLeftRightStyle = { 
+  left: string | number, 
+  right: string 
+}
+
 export class SliderField extends React.PureComponent<
   SliderFieldProps,
   SliderFieldState
@@ -47,154 +52,189 @@ export class SliderField extends React.PureComponent<
   }
 
   componentDidMount(): void {
-    if (this.props.defaultValue === undefined) {
+    const { defaultValue, min, max } = this.props
+
+    if (defaultValue === undefined) {
       this.setState((prevState) => {
         return {
           ...prevState,
-          from: !isNaN(this.props.min) ? this.props.min : prevState.from,
-          to: !isNaN(this.props.max) ? this.props.max : prevState.to
+          from: !isNaN(min) ? min : prevState.from,
+          to: !isNaN(max) ? max : prevState.to
         }
       })
     } else {
-      if (Array.isArray(this.props.defaultValue)) {
+      if (Array.isArray(defaultValue)) {
         this.setState({
-          from: this.props.defaultValue[0],
-          to: this.props.defaultValue[1]
+          from: defaultValue[0],
+          to: defaultValue[1]
         })
       } else {
         this.setState({
-          to: Number(this.props.defaultValue)
+          to: Number(defaultValue)
         })
       }
     }
   }
 
+
+
+  handleChangeFrom = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target
+    const { onChange } = this.props
+
+    this.setState(
+      (prevState) => {
+        return {
+          ...prevState,
+          from: Math.min(Number(input.value), prevState.to),
+          lastInteraction: SliderLastInteraction.from
+        }
+      },
+      () => {
+        if (onChange) {
+          onChange(e, [this.state.from, this.state.to])
+        }
+      }
+    )
+  }
+
+  handleChangeTo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target
+    const { onChange, range } = this.props
+
+    this.setState(
+      (prevState) => {
+        return {
+          ...prevState,
+          to: Math.max(Number(input.value), prevState.from),
+          lastInteraction: SliderLastInteraction.to
+        }
+      },
+      () => {
+        if (onChange) {
+          onChange(
+            e,
+            range ? [this.state.from, this.state.to] : this.state.to
+          )
+        }
+      }
+    )
+  }
+
+  handleMouseUp = (e: React.MouseEvent<HTMLInputElement>) => {
+    const { onMouseUp, range } = this.props
+    if (onMouseUp) {
+      onMouseUp(e, range ? [this.state.from, this.state.to] : this.state.to)
+    }
+  }
+
+  getTrackStyles = (min: number, max: number): SliderFieldLeftRightStyle => {
+    const { range } = this.props
+    const { from, to } = this.state
+
+    const diffMaxMin = max - min
+    const trackLeft = range
+      ? `${((from - min) * 100) / diffMaxMin}%`
+      : 0
+    const trackRight = range
+      ? `${((max - to) * 100) / diffMaxMin}%`
+      : `${100 - ((to - min) * 100) / diffMaxMin}%`
+
+    return {
+      left: trackLeft, 
+      right: trackRight
+    }
+  }
+
+  getLabel = () => {
+    const { label, range } = this.props
+    const { from, to } = this.state
+    return label || (range ? `${from} - ${to}` : to)
+  }
+
+  getMarkStyle = (min: number, max: number): SliderFieldLeftRightStyle => {
+    const { from, to } = this.state
+    const diffMaxMin = max - min
+    const leftStyle = `${((from - min) * 100) / diffMaxMin}%`
+    const rightStyle = `${((to - min) * 100) / diffMaxMin}%`
+
+    return {
+      left: leftStyle,
+      right: rightStyle
+    }
+  }
+
   render(): JSX.Element {
-    const { header, className, onChange, onMouseUp, range } = this.props
+    const { header, className, range } = this.props
 
     const min = this.props.min || SliderDefault.MIN
     const max = this.props.max || SliderDefault.MAX
+
+    const { from, to } = this.state
 
     const classes = ['dcl', 'sliderfield']
     if (className) {
       classes.push(className)
     }
 
-    const handleChangeFrom = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const input = e.target
-
-      this.setState(
-        (prevState) => {
-          return {
-            ...prevState,
-            from: Math.min(Number(input.value), prevState.to),
-            lastInteraction: SliderLastInteraction.from
-          }
-        },
-        () => {
-          if (onChange) {
-            onChange(e, [this.state.from, this.state.to])
-          }
-        }
-      )
-    }
-
-    const handleChangeTo = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const input = e.target
-
-      this.setState(
-        (prevState) => {
-          return {
-            ...prevState,
-            to: Math.max(Number(input.value), prevState.from),
-            lastInteraction: SliderLastInteraction.to
-          }
-        },
-        () => {
-          if (onChange) {
-            onChange(
-              e,
-              range ? [this.state.from, this.state.to] : this.state.to
-            )
-          }
-        }
-      )
-    }
-
-    const handleMouseUp = (e: React.MouseEvent<HTMLInputElement>) => {
-      if (onMouseUp) {
-        onMouseUp(e, range ? [this.state.from, this.state.to] : this.state.to)
-      }
-    }
-
-    const diffMaxMin = max - min
-    const trackLeft = range
-      ? `${((this.state.from - min) * 100) / diffMaxMin}%`
-      : 0
-    const trackRight = range
-      ? `${((max - this.state.to) * 100) / diffMaxMin}%`
-      : `${100 - ((this.state.to - min) * 100) / diffMaxMin}%`
-    const handleFrom = `${((this.state.from - min) * 100) / diffMaxMin}%`
-    const handleTo = `${((this.state.to - min) * 100) / diffMaxMin}%`
-
-    const label =
-      this.props.label ||
-      (range ? `${this.state.from} - ${this.state.to}` : this.state.to)
+    const trackStyle = this.getTrackStyles(min, max)
+    const markStyle = this.getMarkStyle(min, max)
+    const label = this.getLabel()
 
     return (
       <div>
         <div className={classes.join(' ')}>
-          <div className={'dcl sliderfield-header'}>{header}</div>
+          <div className="dcl sliderfield-header">{header}</div>
           <p>{label}</p>
           <div className="dcl sliderfield-wrapper">
-            <div className={'dcl sliderfield-rail'}>
+            <div className="dcl sliderfield-rail">
               <div
-                className={'dcl sliderfield-track'}
-                style={{ left: trackLeft, right: trackRight }}
+                className="dcl sliderfield-track"
+                style={{ left: trackStyle.left, right: trackStyle.right }}
               ></div>
               {range && (
                 <span
-                  className={'dcl sliderfield-handle'}
-                  style={{ left: handleFrom }}
+                  className="dcl sliderfield-mark"
+                  style={{ left: markStyle.left }}
                 ></span>
               )}
 
               <span
-                className={'dcl sliderfield-handle'}
-                style={{ left: handleTo }}
+                className="dcl sliderfield-mark"
+                style={{ left: markStyle.right }}
               ></span>
             </div>
 
             {range && (
               <input
                 type="range"
-                value={this.state.from}
+                value={from}
                 max={max}
                 min={min}
                 step="1"
-                onChange={handleChangeFrom}
+                onChange={this.handleChangeFrom}
                 style={{
                   zIndex:
                     this.state.lastInteraction == SliderLastInteraction.from
                       ? 4
                       : 3
                 }}
-                onMouseUp={handleMouseUp}
+                onMouseUp={this.handleMouseUp}
               />
             )}
 
             <input
               type="range"
-              value={this.state.to}
+              value={to}
               max={max}
               min={min}
               step="1"
-              onChange={handleChangeTo}
+              onChange={this.handleChangeTo}
               style={{
                 zIndex:
                   this.state.lastInteraction == SliderLastInteraction.to ? 4 : 3
               }}
-              onMouseUp={handleMouseUp}
+              onMouseUp={this.handleMouseUp}
             />
           </div>
         </div>
