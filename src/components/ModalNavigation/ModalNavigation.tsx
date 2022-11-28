2a644b09b473b62d241d08759a0bf3528f37fdf5
Florencia Barreto <florbarrlad@gmail.com>
2022-11-28 14:19:45 -0300

    feat: mobile ui version

--- src/components/ModalNavigation/ModalNavigation.tsx
+++ src/components/ModalNavigation/ModalNavigation.tsx
@@ -2,15 +2,21 @@ import * as React from 'react'
 import './ModalNavigation.css'
 
 export type ModalNavigationProps = {
+  // * TITLE: modal's title
   title: React.ReactNode
+  // * SUBTITLE: modal's subtitle, optional
   subtitle?: React.ReactNode
+  // * ON BACK: on back event, this allows navigation inside modal and renders a button arrow
   onBack?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+  // * ON CLOSE: on clse event, this allows closing the modal rendering a close button
   onClose?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+  // * ON INFO: on info event, this renders a clickable question mark
+  onInfo?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
 }
 
 export class ModalNavigation extends React.PureComponent<ModalNavigationProps> {
   render(): JSX.Element {
-    const { title, subtitle, onBack, onClose } = this.props
+    const { title, subtitle, onBack, onClose, onInfo } = this.props
     return (
       <div className="dcl modal-navigation">
         <div className="dcl modal-navigation-title">{title}</div>
@@ -21,6 +27,12 @@ export class ModalNavigation extends React.PureComponent<ModalNavigationProps> {
           <div
             className="dcl modal-navigation-button modal-navigation-back"
             onClick={onBack}
+          />
+        )}
+        {onInfo && (
+          <div
+            className="dcl modal-navigation-button modal-navigation-info"
+            onClick={onInfo}
           />
         )}
         {onClose && (
