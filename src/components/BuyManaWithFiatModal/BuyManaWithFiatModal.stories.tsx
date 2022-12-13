5ad5daeb3e2efb469cc8967132ab91d27697cbe1
Kevin Szuchet <kevin.szuchet@decentraland.org>
2022-12-13 12:57:15 -0300

    feat: Create new component with the last step of Buy Mana with Fiat

--- src/components/BuyManaWithFiatModal/BuyManaWithFiatModal.stories.tsx
+++ src/components/BuyManaWithFiatModal/BuyManaWithFiatModal.stories.tsx
@@ -6,6 +6,7 @@ import { Header } from '../Header/Header'
 import { BuyManaWithFiatModal } from '../BuyManaWithFiatModal/BuyManaWithFiatModal'
 import { NetworkGatewayType } from './Network'
 import './BuyManaWithFiatModal.stories.css'
+import { FeedbackModal } from './FeedbackModal'
 
 const lipsum = (
   <>
@@ -49,7 +50,7 @@ const lipsum = (
 
 storiesOf('BuyManaWithFiatModal', module)
   .addDecorator(centered)
-  .add('BuyWithFiat options with default values', () => (
+  .add('BuyManaWithFiat options with default values', () => (
     <div className="BuyManaWithFiatModal-story">
       {lipsum}
       <BuyManaWithFiatModal
@@ -84,7 +85,7 @@ storiesOf('BuyManaWithFiatModal', module)
       />
     </div>
   ))
-  .add('BuyWithFiat options with custom texts', () => (
+  .add('BuyManaWithFiat options with custom texts', () => (
     <div className="BuyManaWithFiatModal-story">
       {lipsum}
       <BuyManaWithFiatModal
@@ -136,29 +137,32 @@ storiesOf('BuyManaWithFiatModal', module)
       />
     </div>
   ))
-  .add('BuyWithFiat with Polygon already selected and only one gateway', () => (
-    <div className="BuyManaWithFiatModal-story">
-      {lipsum}
-      <BuyManaWithFiatModal
-        open
-        networks={[
-          {
-            type: Network.MATIC,
-            gateways: [
-              {
-                type: NetworkGatewayType.TRANSAK,
-                learnMoreLink: 'https://transak.com/',
-                onContinue: () => undefined
-              }
-            ]
-          }
-        ]}
-        onClose={() => undefined}
-        onInfo={() => undefined}
-      />
-    </div>
-  ))
-  .add('BuyWithFiat with Ethereum already selected', () => (
+  .add(
+    'BuyManaWithFiat with Polygon already selected and only one gateway',
+    () => (
+      <div className="BuyManaWithFiatModal-story">
+        {lipsum}
+        <BuyManaWithFiatModal
+          open
+          networks={[
+            {
+              type: Network.MATIC,
+              gateways: [
+                {
+                  type: NetworkGatewayType.TRANSAK,
+                  learnMoreLink: 'https://transak.com/',
+                  onContinue: () => undefined
+                }
+              ]
+            }
+          ]}
+          onClose={() => undefined}
+          onInfo={() => undefined}
+        />
+      </div>
+    )
+  )
+  .add('BuyManaWithFiat with Ethereum already selected', () => (
     <div className="BuyManaWithFiatModal-story">
       {lipsum}
       <BuyManaWithFiatModal
@@ -185,7 +189,13 @@ storiesOf('BuyManaWithFiatModal', module)
       />
     </div>
   ))
-  .add('BuyWithFiat persisent message', () => (
+  .add('BuyManaWithFiat last step: Feedback Modal', () => (
+    <div className="BuyManaWithFiatModal-story">
+      {lipsum}
+      <FeedbackModal open onClose={() => undefined} onInfo={() => undefined} />
+    </div>
+  ))
+  .add('BuyManaWithFiat persisent message', () => (
     <div className="BuyManaWithFiatModal-story">
       {lipsum}
       <BuyManaWithFiatModal
@@ -200,7 +210,7 @@ storiesOf('BuyManaWithFiatModal', module)
       />
     </div>
   ))
-  .add('BuyWithFiat error', () => (
+  .add('BuyManaWithFiat error', () => (
     <div className="BuyManaWithFiatModal-story">
       {lipsum}
       <BuyManaWithFiatModal
---
+++ src/components/BuyManaWithFiatModal/FeedbackModal.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react'
+import ModalContent from 'semantic-ui-react/dist/commonjs/modules/Modal/ModalContent'
+import classNames from 'classnames'
+import FeedbackStarts from '../../assets/feedback-stars.svg'
+import { Modal } from '../Modal/Modal'
+import { ModalNavigation } from '../ModalNavigation/ModalNavigation'
+import { Loader } from '../Loader/Loader'
+import { Button } from '../Button/Button'
+import './FeedbackModal.css'
+
+export type FeedbackModalProps = {
+  className?: string
+  message?: React.ReactNode
+  hasError?: boolean
+  open?: boolean
+  loading?: boolean
+  i18n?: FeedbackModalI18N
+  onClickCta?: () => void
+  onClose?: () => void
+  onInfo?: () => void
+}
+
+export type FeedbackModalI18N = {
+  title: React.ReactNode
+  description: React.ReactNode
+  cta: React.ReactNode
+  error: React.ReactNode
+}
+
+export class FeedbackModal extends React.Component<FeedbackModalProps> {
+  static defaultProps = {
+    className: '',
+    hasError: false,
+    loading: false,
+    i18n: {
+      title: 'Awesome',
+      description:
+        'You just bought some mana you naughty friend.\nThis copy clearly needs review',
+      cta: 'Done',
+      error: 'Could not process the transaction'
+    }
+  }
+
+  render(): JSX.Element {
+    const {
+      open,
+      className,
+      message,
+      i18n,
+      hasError,
+      loading,
+      onClose,
+      onInfo,
+      onClickCta
+    } = this.props
+
+    let errorClasses = 'error'
+    if (this.props.hasError) {
+      errorClasses += ' visible'
+    }
+
+    return (
+      <Modal
+        open={open}
+        className={classNames('dcl', 'feedback-modal', className)}
+      >
+        <ModalNavigation title={i18n.title} onInfo={onInfo} onClose={onClose} />
+        <ModalContent>
+          <img src={FeedbackStarts} className="stars" />
+          <p>{i18n.description}</p>
+          {message ? <small className="message">{message}</small> : null}
+        </ModalContent>
+        {hasError ? <p className={errorClasses}>{i18n.error}</p> : null}
+        {loading ? (
+          <>
+            <Loader size="big" active />
+            <div className="loader-background"></div>
+          </>
+        ) : null}
+        <Modal.Actions>
+          <Button primary onClose={onClickCta || onClose}>
+            {i18n.cta}
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    )
+  }
+}
