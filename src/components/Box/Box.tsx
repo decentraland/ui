3928f6b48cdf064dd11100adf3a8079fa4d11aa8
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2022-12-20 18:13:00 -0300

    feat: add collapsible prop to box

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,30 +1,64 @@
 import * as React from 'react'
+import classnames from 'classnames'
+import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon/Icon'
 import './Box.css'
+import { Button } from 'semantic-ui-react'
 
 export type BoxProps = {
   header?: string
   className?: string
   children: React.ReactNode
   borderless?: boolean
+  collapsible?: boolean
+  collapsed?: boolean
+  onToggle?: (shouldCollapse: boolean) => void
 }
 
-export class Box extends React.PureComponent<BoxProps> {
-  render(): JSX.Element {
-    const { className, header, children, borderless } = this.props
+export const Box = (props: BoxProps): JSX.Element => {
+  const {
+    collapsed,
+    header,
+    children,
+    borderless,
+    className,
+    collapsible,
+    onToggle
+  } = props
+  const [isCollapsed, setIsCollapsed] = React.useState(
+    'collapsed' in props ? collapsed : false
+  )
 
-    const classes = ['dcl', 'box']
-    if (className) {
-      classes.push(className)
-    }
-    if (borderless) {
-      classes.push('borderless')
-    }
+  const shouldCollapse = 'collapsed' in props ? collapsed : isCollapsed
 
-    return (
-      <div className={classes.join(' ')}>
-        {header && <div className={'dcl box-header'}>{header}</div>}
-        <div className={'dcl box-children'}>{children}</div>
-      </div>
-    )
+  function handleToggleCollapse() {
+    if (onToggle) {
+      onToggle(!isCollapsed)
+    }
+    setIsCollapsed(!isCollapsed)
   }
+
+  return (
+    <div className={classnames('dcl box', className, { borderless })}>
+      {header && (
+        <div
+          className={classnames('box-header', { collapsed: shouldCollapse })}
+        >
+          <div className="dcl box-header-text">{header}</div>
+          {collapsible && (
+            <Button
+              basic
+              className="collapse-btn"
+              onClick={handleToggleCollapse}
+            >
+              <Icon
+                name={shouldCollapse ? 'chevron down' : 'chevron up'}
+                color="yellow"
+              />
+            </Button>
+          )}
+        </div>
+      )}
+      {!shouldCollapse && <div className={'dcl box-children'}>{children}</div>}
+    </div>
+  )
 }
