f6be02f02fece4e825f76790a52e1969e1b90e69
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-01-31 09:28:24 -0300

    feat: Add flag to Mana component allowing changing it to primary

--- src/components/Mana/Mana.tsx
+++ src/components/Mana/Mana.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Network } from '@dcl/schemas/dist/dapps/network'
 import { Header, HeaderProps } from '../Header/Header'
 import './Mana.css'
+import classNames from 'classnames'
 
 export type ManaProps = {
   size?: 'tiny' | 'small' | 'medium' | 'large' | 'huge'
@@ -9,21 +10,38 @@ export type ManaProps = {
   network?: Network
   className?: string
   children?: React.ReactChild
+  primary?: boolean
 }
 
 export class Mana extends React.Component<ManaProps & HeaderProps> {
   static defaultProps = {
     className: '',
-    network: Network.ETHEREUM
+    network: Network.ETHEREUM,
+    primary: false
   }
 
   render(): JSX.Element {
-    const { size, className, inline, children, network, ...rest } = this.props
-    const classes = `dcl mana ${inline ? 'inline ' : ''}${className}`.trim()
+    const { size, className, inline, children, network, primary, ...rest } =
+      this.props
+
     return (
-      <Header size={size} className={classes} {...rest}>
+      <Header
+        size={size}
+        className={classNames(
+          'dcl',
+          'mana',
+          inline ? 'inline ' : null,
+          className
+        )}
+        {...rest}
+      >
         <i className="symbol">
-          <i className={network.toLowerCase()} />
+          <i
+            className={classNames(
+              network.toLowerCase(),
+              primary ? 'primary' : null
+            )}
+          />
         </i>
         {children}
       </Header>
