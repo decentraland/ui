c0e49829c701229bb2a4453195dacb95859b92fc
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-03-30 21:24:37 -0300

    reset babel update

--- src/components/NetworkAlert/NetworkAlert.stories.tsx
+++ src/components/NetworkAlert/NetworkAlert.stories.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
-import centered from '@storybook/addon-centered/react'
 
 import { Navbar } from '../Navbar/Navbar'
 import { NetworkAlert } from './NetworkAlert'
 import '../Navbar/Navbar.stories.css'
 
 storiesOf('NetworkAlert', module)
-  .addDecorator(centered)
   .add('Partially supported network', () => {
     return (
       <div className="Navbar-story-container">
