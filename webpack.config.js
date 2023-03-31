d7d23cb6aa87628fff3dffa404af880f3c9b024b
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-03-31 12:04:04 -0300

    fix: [object Module] error in webpack (#316)

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
             loader: 'url-loader',
             options: {
               limit: 8192,
-              options: { esModule: false }
+              esModule: false
             }
           }
         ]
