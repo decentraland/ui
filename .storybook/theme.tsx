46869f582635a0340add07ace3c09784c9fadff5
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-04-14 20:02:53 +0200

    fix: Run prettier fix

--- .storybook/theme.tsx
+++ .storybook/theme.tsx
@@ -92,7 +92,7 @@ const DARK_THEME = `
   --navbar-popup-hover: #67637033;
   --card: #242129;
   --outline: 1px solid #00000005;
-  --toast: #3D3B43;
+  --toast: #3d3b43;
   --toast-text: #ffffff;
   --modal: #242129;
   --dimmer: #000000dd;
