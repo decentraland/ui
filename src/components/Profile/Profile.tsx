d877109428474c8513b3521dc522243e0c56dcb5
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2023-04-25 18:40:49 +0200

    fix: Avoid a default value for the generic (#339)

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,126 +1,119 @@
 import * as React from 'react'
 import { Avatar } from '@dcl/schemas/dist/platform/profile/avatar'
-import { WrappedAsProps } from '../../types/as'
 import { Logo } from '../Logo/Logo'
 import { Popup } from '../Popup/Popup'
 import { AvatarFace } from '../AvatarFace/AvatarFace'
 import { Blockie } from '../Blockie/Blockie'
 import './Profile.css'
 
-export type ProfileProps<T extends React.ElementType = typeof React.Fragment> =
-  {
-    address: string
-    avatar?: Avatar | null
-    textOnly?: boolean
-    imageOnly?: boolean
-    hasPopup?: boolean
-    inline?: boolean
-    sliceAddressBy?: number
-    size?: 'normal' | 'large' | 'huge' | 'massive'
-    isDecentraland?: boolean
-  } & WrappedAsProps<T>
+type Props<T extends React.ElementType> = {
+  address: string
+  avatar?: Avatar | null
+  textOnly?: boolean
+  imageOnly?: boolean
+  hasPopup?: boolean
+  inline?: boolean
+  sliceAddressBy?: number
+  size?: 'normal' | 'large' | 'huge' | 'massive'
+  isDecentraland?: boolean
+  as?: T
+}
 
-export class Profile<T extends React.ElementType> extends React.PureComponent<
-  ProfileProps<T>
-> {
-  static defaultProps = {
-    inline: true,
-    sliceAddressBy: 6,
-    size: 'normal',
-    as: React.Fragment
+export type ProfileProps<T extends React.ElementType> = Props<T> &
+  Omit<React.ComponentPropsWithRef<T>, keyof Props<T>>
+
+export const Profile = function <T extends React.ElementType>(
+  props: ProfileProps<T>
+) {
+  const {
+    address,
+    avatar,
+    textOnly,
+    imageOnly,
+    hasPopup,
+    inline = true,
+    size = 'normal',
+    sliceAddressBy = 6,
+    isDecentraland,
+    as = React.Fragment,
+    ...rest
+  } = props
+
+  const sliceLimit = Math.max(Math.min(sliceAddressBy, 42), 6)
+  const name = (avatar && avatar.name) || address.slice(0, sliceLimit)
+  const Wrapper = as
+
+  if (isDecentraland) {
+    return (
+      <span
+        className={`Profile decentraland ${size} ${inline ? 'inline' : ''}`}
+        title={address}
+      >
+        <Wrapper {...rest}>
+          <Logo />
+        </Wrapper>
+        {imageOnly ? null : (
+          <Wrapper {...rest}>
+            <span className="name">Decentraland</span>
+          </Wrapper>
+        )}
+      </span>
+    )
   }
 
-  render(): React.ReactNode {
-    const {
-      address,
-      avatar,
-      textOnly,
-      imageOnly,
-      hasPopup,
-      inline,
-      size,
-      sliceAddressBy,
-      isDecentraland,
-      as,
-      ...rest
-    } = this.props
-
-    const sliceLimit = Math.max(Math.min(sliceAddressBy, 42), 6)
-    const name = (avatar && avatar.name) || address.slice(0, sliceLimit)
-    const Wrapper = as
-
-    if (isDecentraland) {
-      return (
-        <span
-          className={`Profile decentraland ${size} ${inline ? 'inline' : ''}`}
-          title={address}
-        >
-          <Wrapper {...rest}>
-            <Logo />
-          </Wrapper>
-          {imageOnly ? null : (
-            <Wrapper {...rest}>
-              <span className="name">Decentraland</span>
-            </Wrapper>
-          )}
-        </span>
-      )
-    }
-
-    if (textOnly) {
-      return name
-    } else {
-      return (
-        <Popup
-          content={name}
-          disabled={!hasPopup}
-          position="top center"
-          trigger={
-            avatar ? (
-              <span
-                className={`Profile avatar ${size} ${inline ? 'inline' : ''}`}
-                title={address}
-              >
+  if (textOnly) {
+    return <>{name}</>
+  } else {
+    return (
+      <Popup
+        content={name}
+        disabled={!hasPopup}
+        position="top center"
+        trigger={
+          avatar ? (
+            <span
+              className={`Profile avatar ${size} ${inline ? 'inline' : ''}`}
+              title={address}
+            >
+              <Wrapper {...rest}>
+                <AvatarFace size="tiny" inline={inline} avatar={avatar} />
+              </Wrapper>
+              {imageOnly ? null : (
                 <Wrapper {...rest}>
-                  <AvatarFace size="tiny" inline={inline} avatar={avatar} />
+                  <span className="name">{name}</span>
                 </Wrapper>
-                {imageOnly ? null : (
-                  <Wrapper {...rest}>
-                    <span className="name">{name}</span>
-                  </Wrapper>
-                )}
-              </span>
-            ) : (
-              <span
-                className={`Profile blockie ${size} ${inline ? 'inline' : ''}`}
-                title={address}
-              >
+              )}
+            </span>
+          ) : (
+            <span
+              className={`Profile blockie ${size} ${inline ? 'inline' : ''}`}
+              title={address}
+            >
+              <Wrapper {...rest}>
+                <Blockie
+                  seed={address}
+                  scale={
+                    size === 'large'
+                      ? 5
+                      : size === 'huge'
+                      ? 7
+                      : size === 'massive'
+                      ? 21
+                      : 3
+                  }
+                  as={as}
+                  {...rest}
+                />
+              </Wrapper>
+              {imageOnly ? null : (
                 <Wrapper {...rest}>
-                  <Blockie
-                    seed={address}
-                    scale={
-                      size === 'large'
-                        ? 5
-                        : size === 'huge'
-                        ? 7
-                        : size === 'massive'
-                        ? 21
-                        : 3
-                    }
-                    as={as}
-                    {...rest}
-                  />
+                  <span className="name">{name}</span>
                 </Wrapper>
-                {imageOnly ? null : (
-                  <Wrapper {...rest}>
-                    <span className="name">{name}</span>
-                  </Wrapper>
-                )}
-              </span>
-            )
-          }
-        />
-      )
-    }
+              )}
+            </span>
+          )
+        }
+      />
+    )
   }
 }
--- src/types/as.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export type WrappedAsProps<
-  T extends React.ElementType = typeof React.Fragment
-> = {
-  as?: T
-} & { children?: React.ReactNode } & React.ComponentPropsWithoutRef<T>
