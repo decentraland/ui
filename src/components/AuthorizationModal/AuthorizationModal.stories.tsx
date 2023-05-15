151d908e21c7f47d3536ea0855fbe5554e6f46db
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-05-15 01:45:26 -0300

    fix: authorization error icon

---
+++ src/components/AuthorizationModal/AuthorizationModal.stories.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { AuthorizationModal } from './AuthorizationModal'
+import { AuthorizationStep } from './AuthorizationModal.types'
+import { AuthorizationStepStatus } from './AuthorizationModal.types'
+import { AuthorizationStepAction } from './AuthorizationModal.types'
+
+const steps: AuthorizationStep[] = [
+  {
+    title: 'Revoke',
+    onActionClick: () => console.log('revoke'),
+    message: <span className="authorization-error">This is a big error message</span>,
+    status: AuthorizationStepStatus.ERROR,
+    actionType: AuthorizationStepAction.REVOKE,
+    action: "Revoke"
+  }
+]
+
+storiesOf('AuthorizationModal', module).add('Basic with error', () => (
+  <AuthorizationModal
+    header="Authorization"
+    currentStep={0}
+    steps={steps}
+    onClose={() => console.log('Close')}
+  />
+))
