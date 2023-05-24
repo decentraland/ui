556b712bf0dfd925efa6dd06ad58a61b53cb345b
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2023-05-24 14:25:42 -0300

    feat: Make field count the text length when maxLength is set (#365)
    * feat: Make field count the text length when maxLength is set
    
    * fix: Allow maxLength to be shown without a label
    
    * fix: Remove justify
    
    * fix: Use function to get value length

--- src/components/Field/Field.stories.tsx
+++ src/components/Field/Field.stories.tsx
@@ -122,3 +122,9 @@ storiesOf('Field', module)
       />
     </Form>
   ))
+  .add('With maxLength and no label', () => (
+    <Field maxLength={20} value="A value" />
+  ))
+  .add('With maxLength and label', () => (
+    <Field label="A label" maxLength={20} value="A value" />
+  ))
