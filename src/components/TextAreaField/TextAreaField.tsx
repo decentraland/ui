a9641c6fb0bd969055ead0ed9841834accb613b8
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2023-05-24 15:21:09 -0300

    feat: Add max length count for the TextArea component (#366)
    * feat: Add max length count
    
    * fix: Use lib function

--- src/components/TextAreaField/TextAreaField.tsx
+++ src/components/TextAreaField/TextAreaField.tsx
@@ -2,9 +2,13 @@ import * as React from 'react'
 import TextArea, {
   TextAreaProps
 } from 'semantic-ui-react/dist/commonjs/addons/TextArea/TextArea'
+import { getInputValueLength } from '../../lib/input'
 import './TextAreaField.css'
 
-export type TextAreaFieldProps = TextAreaProps & { label?: string }
+export type TextAreaFieldProps = TextAreaProps & {
+  label?: string
+  maxLength?: number
+}
 
 export const TextAreaField = (props: TextAreaFieldProps): JSX.Element => {
   const textAreaProps = { ...props }
@@ -12,7 +16,17 @@ export const TextAreaField = (props: TextAreaFieldProps): JSX.Element => {
 
   return (
     <div className="dcl text-area">
-      {props.label && <div className="label">{props.label}</div>}
+      {props.label || props.maxLength !== undefined ? (
+        <div className="title">
+          {props.label ? <label>{props.label}</label> : null}
+          {props.maxLength ? (
+            <span className="maxLength">
+              {getInputValueLength(props.value)}/{props.maxLength}
+            </span>
+          ) : null}
+        </div>
+      ) : null}
+
       <TextArea {...textAreaProps} />
     </div>
   )
