6244cc60ba34e76868acea4b94a9e6f50212777b
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-09-07 15:01:43 -0300

    fix: asset image css

--- src/components/AssetImage/AssetImage.tsx
+++ src/components/AssetImage/AssetImage.tsx
@@ -18,7 +18,7 @@ export const AssetImage = (props: AssetImageProps) => {
 
   return (
     <div
-      className={classNames('AssetImage', className)}
+      className={classNames('uiAssetImage', className)}
       style={{
         backgroundImage
       }}
