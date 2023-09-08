6aaa98078991ed803fe31947c0fea7922d241ddb
Nando <24811313+fzavalia@users.noreply.github.com>
2023-09-08 12:14:40 -0300

    fix: Don't use storiesOf

--- src/components/CategoryFilter/CategoryFilter.stories.tsx
+++ src/components/CategoryFilter/CategoryFilter.stories.tsx
@@ -1,30 +1,71 @@
-import * as React from 'react'
-import { storiesOf } from '@storybook/react'
+import React from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
 import { CategoryFilter } from './CategoryFilter'
 
-storiesOf('CategoryFilter', module).add('Basic Filter', () => {
-  return (
-    <CategoryFilter
-      title="categories"
-      value="wearables"
-      items={[
+export default {
+  title: 'CategoryFilter',
+  component: CategoryFilter
+} as ComponentMeta<typeof CategoryFilter>
+
+const Template: ComponentStory<typeof CategoryFilter> = (args) => (
+  <CategoryFilter {...args} />
+)
+
+export const Primary = Template.bind({})
+Primary.args = {
+  title: 'categories',
+  value: 'head',
+  items: [
+    {
+      id: 'wearables',
+      label: 'Wearables',
+      children: [
         {
-          id: 'wearables',
-          label: 'Wearables',
+          id: 'head',
+          label: 'Head',
           children: [
             {
-              id: 'head',
-              label: 'Head',
-              children: [
-                {
-                  id: 'facial-hair',
-                  label: 'Facial Hair'
-                }
-              ]
+              id: 'facial-hair',
+              label: 'Facial Hair'
+            },
+            {
+              id: 'hair',
+              label: 'Hair'
+            },
+            {
+              id: 'eyes',
+              label: 'Eyes'
             }
           ]
+        },
+        {
+          id: 'upper-body',
+          label: 'Upper Body'
+        },
+        {
+          id: 'hand-wear',
+          label: 'Hand Wear'
         }
-      ]}
-    />
-  )
-})
+      ]
+    },
+    {
+      id: 'emotes',
+      label: 'Emotes',
+      children: [
+        {
+          id: 'dance',
+          label: 'Dance'
+        },
+        {
+          id: 'poses',
+          label: 'Poses'
+        },
+        {
+          id: 'fun',
+          label: 'Fun'
+        }
+      ]
+    }
+  ]
+}
