aeb63a2b8632b3b66bf0747322350f566d2364df
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-01-23 14:27:07 -0300

    fix: address field prop types (#499)

--- src/components/AddressField/AddressField.types.ts
+++ src/components/AddressField/AddressField.types.ts
@@ -3,5 +3,5 @@ import { FieldProps } from '../Field/Field'
 export type Props = FieldProps & {
   fieldClassName?: string
   i18n?: { errorMessage: string }
-  resolveName: (address: string) => string | undefined
+  resolveName: (address: string) => Promise<string | undefined>
 }
