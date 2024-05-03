6c3a2b948b64baa2ab60ee8842985014ece167cb
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2024-05-03 11:23:08 -0300

    feat: Add utility icon to IconBadge (#534)

--- src/components/IconBadge/IconBadge.stories.tsx
+++ src/components/IconBadge/IconBadge.stories.tsx
@@ -5,8 +5,8 @@ import { IconBadge } from './IconBadge'
 storiesOf('IconBadge', module)
   .add('With Icon', () => (
     <IconBadge
-      icon="BaseMale"
-      text="Male Icon"
+      icon="utility"
+      text="Utility"
       onClick={() => console.log('Clicked!')}
     />
   ))
@@ -14,6 +14,9 @@ storiesOf('IconBadge', module)
     <IconBadge text="Custom Icon">
       <div>Custom Icon Component</div>
     </IconBadge>
+  ))
+  .add('Without text', () => (
+    <IconBadge icon="BaseFemale" onClick={() => console.log('Clicked!')} />
   ))
   .add('Without Icon', () => (
     <IconBadge text="No Icon" onClick={() => console.log('Clicked!')} />
