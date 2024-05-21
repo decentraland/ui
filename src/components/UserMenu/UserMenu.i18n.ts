3d3116ffaaa0bada0f8a3553257d3cc869c58d11
Braian Mellor <braianj@gmail.com>
2024-05-21 12:25:45 -0300

    feat: sort UserMenuI18N
    BREAKING CHANGE: rename functions in UserMenu props

--- src/components/UserMenu/UserMenu.i18n.ts
+++ src/components/UserMenu/UserMenu.i18n.ts
@@ -1,15 +1,15 @@
 import { UserMenuI18N } from './UserMenu.types'
 
 export const i18n = {
+  account: 'Account Settings',
   activity: 'Activity',
+  guest: 'Guest',
+  jumpIn: 'Jump In',
+  marketplaceAuthorizations: 'Marketplace Authorizations',
   myAssets: 'My Assets',
   myLists: 'My Lists',
-  account: 'Account Settings',
   profile: 'Profile',
-  viewProfile: 'View Profile',
   signIn: 'Sign In',
   signOut: 'Log Out',
-  guest: 'Guest',
-  marketplaceAuthorizations: 'Marketplace Authorizations',
-  jumpIn: 'Jump In'
+  viewProfile: 'View Profile'
 } as UserMenuI18N
