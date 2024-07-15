832907f212586a9bf253746fcdccd1df1ad50f4c
Braian Mellor <braianj@gmail.com>
2024-07-15 15:17:14 -0300

    feat: change way to generate NavbarPagesUrl

--- src/components/Navbar/MainMenu/MainMenu.tsx
+++ src/components/Navbar/MainMenu/MainMenu.tsx
@@ -4,12 +4,14 @@ import classNames from 'classnames'
 import { MenuItem } from '../MenuItem/MenuItem'
 import { NavbarPages } from '../Navbar.types'
 import { MainMenuProps } from './MainMenu.types'
-import { NavbarPagesUrl } from '../utils'
+import { getNavbarPagesUrls } from '../utils'
 
 import './MainMenu.css'
 
 export const MainMenu = (props: MainMenuProps) => {
   const { i18n, ...menuItemProps } = props
+
+  const urls = getNavbarPagesUrls()
 
   return (
     <div
@@ -23,31 +25,31 @@ export const MainMenu = (props: MainMenuProps) => {
         {...menuItemProps}
         section={NavbarPages.MARKETPLACE}
         title={i18n.marketplace}
-        mainUrl={NavbarPagesUrl(NavbarPages.MARKETPLACE)}
+        mainUrl={urls[NavbarPages.MARKETPLACE]}
       />
       <MenuItem
         {...menuItemProps}
         section={NavbarPages.CREATE}
         title={i18n.create}
-        mainUrl={NavbarPagesUrl(NavbarPages.CREATE)}
+        mainUrl={urls[NavbarPages.CREATE]}
       />
       <MenuItem
         {...menuItemProps}
         section={NavbarPages.EXPLORE}
         title={i18n.explore}
-        mainUrl={NavbarPagesUrl(NavbarPages.EXPLORE)}
+        mainUrl={urls[NavbarPages.EXPLORE]}
       />
       <MenuItem
         {...menuItemProps}
         section={NavbarPages.LEARN}
         title={i18n.learn}
-        mainUrl={NavbarPagesUrl(NavbarPages.LEARN)}
+        mainUrl={urls[NavbarPages.LEARN]}
       />
       <MenuItem
         {...menuItemProps}
         section={NavbarPages.GOVERNANCE}
         title={i18n.governance}
-        mainUrl={NavbarPagesUrl(NavbarPages.GOVERNANCE)}
+        mainUrl={urls[NavbarPages.GOVERNANCE]}
       />
     </div>
   )
