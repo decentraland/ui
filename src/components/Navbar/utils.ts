904e8be4d497b1bc1e1fa8d69b30b49629810baa
Juan Cazala <juancazala@gmail.com>
2024-11-13 12:39:05 -0300

    chore: move contentful stuff to config

--- src/components/Navbar/utils.ts
+++ src/components/Navbar/utils.ts
@@ -41,10 +41,10 @@ export const getExtraButton = async () => {
     }
   }
   try {
-    const SPACE_ID = 'ea2ybdmmn1kv'
-    const ENV = 'master'
-    const ACCESS_TOKEN = '9dieh3AHS6uwb_YNMjxlO6FCibAFFJVdg2YzA5t6U-Y'
-    const ENTRY_ID = '18g1DzIyBxvu0steSwKyQr'
+    const SPACE_ID = config.get('CONTENTFUL_SPACE_ID')
+    const ENV = config.get('CONTENTFUL_ENV')
+    const ACCESS_TOKEN = config.get('CONTENTFUL_NAVBAR_ACCESS_TOKEN')
+    const ENTRY_ID = config.get('CONTENTFUL_NAVBAR_ENTRY_ID')
     const CONTENTFUL_URL = `https://cdn.contentful.com/spaces/${SPACE_ID}/environments/${ENV}/entries/${ENTRY_ID}?access_token=${ACCESS_TOKEN}`
     const response = await fetch(CONTENTFUL_URL)
     const entry = await response.json()
