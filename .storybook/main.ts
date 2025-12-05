0c69aabbc9cef2d6bbed9845c6d167bf5694d677
Braian Mellor <braianj@gmail.com>
2025-12-05 10:48:35 -0300

    feat: migrate Storybook from 6.5.16 to 7.6.20 and update React to 18
    - Update React and ReactDOM to ^18.0.0
    - Update @types/react, @types/react-dom, @types/react-test-renderer to ^18.0.0
    - Update all Storybook packages to 7.6.20
    - Migrate from @storybook/builder-webpack4 to @storybook/builder-webpack5
    - Migrate from @storybook/react to @storybook/react-webpack5
    - Update Storybook configuration for v7
    - Remove --openssl-legacy-provider from Storybook scripts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,18 @@ const webpackConfig = require('../webpack.config.js');
 const cssRule = webpackConfig.module.rules[0]
 
 module.exports = {
+  stories: [
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    '@storybook/addon-storysource',
+    '@storybook/addon-a11y',
+    '@storybook/addon-essentials'
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
   webpackFinal: async (config) => {
     config.externals = ["react-dom/client"]
     config.module.rules = [
@@ -19,14 +31,5 @@ module.exports = {
     ]
     return { ...config, module: { ...config.module, rules: config.module.rules } };
   },
-  stories: [
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  addons: [
-    '@storybook/addon-storysource',
-    '@storybook/addon-a11y',
-    '@storybook/addon-essentials'
-  ],
-  framework: "@storybook/react"
 }
 
